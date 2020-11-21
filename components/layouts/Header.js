d5ed1d9e7a75c39dec8d79f9de30c55112a8ced5
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-21 20:01:16 +0100

    CSS del Header

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,24 +1,51 @@
 import React from "react";
-import Buscar from '../ui/Buscar';
-import Navegacion from '../layouts/Navegacion';
-import Link from 'next/link';
+import Buscar from "../ui/Buscar";
+import Navegacion from "../layouts/Navegacion";
+import Link from "next/link";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+const ContenedorHeader = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header
+      css={css`
+        border-bottom: 2px solid var(--gris3);
+        padding: 1rem 0;
+      `}
+    >
+      <ContenedorHeader>
         <div>
-          <p>Product Hunt</p>
+            <Link href="/">
+                <Logo>Product Hunt</Logo>
+            </Link>
             <Buscar />
             <Navegacion />
         </div>
         <div>
             <p>Hola: Carlos</p>
-        </div>
             <button type="button">Cerrar sesión</button>
             <Link href="/">Login</Link>
-            <Link href="/">Crear cuenta</Link>
-      </div>
+            <Link href="/">Crear cuenta</Link>          
+        </div>
+      </ContenedorHeader>
     </header>
   );
 };
--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -11,6 +11,7 @@ const Layout = props => {
                     :root {
                         --gris: #3d3d3d;
                         --gris2: #6f6f6f;
+                        --gris3: #e1e1e1;
                         --naranja: #da552f; 
                     }
                     html {
