da1a649fe2f6597b6a29750d7d748a899dc906de
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-21 20:49:48 +0100

    Aplicando estilos a la barra de navegacion

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
-import Buscar from "../ui/Buscar";
-import Navegacion from "../layouts/Navegacion";
 import Link from "next/link";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
+import Buscar from "../ui/Buscar";
+import Navegacion from "../layouts/Navegacion";
+import Boton from '../ui/Boton';
 
 const ContenedorHeader = styled.div`
   max-width: 1200px;
@@ -24,6 +25,9 @@ const Logo = styled.p`
 `;
 
 const Header = () => {
+
+    const usuario = false;
+
   return (
     <header
       css={css`
@@ -39,11 +43,31 @@ const Header = () => {
             <Buscar />
             <Navegacion />
         </div>
-        <div>
-            <p>Hola: Carlos</p>
-            <button type="button">Cerrar sesión</button>
-            <Link href="/">Login</Link>
-            <Link href="/">Crear cuenta</Link>          
+        <div
+            css={css`
+                display: flex;
+                align-items: center;
+            `}
+        >
+            { usuario ? (
+                <>
+                    <p
+                        css={css`
+                            margin-right: 2rem;
+                        `}
+                    >Hola: Carlos</p>
+                    <Boton bgColor="true">Cerrar sesión</Boton>
+                </>
+            ) : (
+                <>
+                    <Link href="/">
+                        <Boton bgColor="true">Login</Boton>
+                    </Link>
+                    <Link href="/">
+                        <Boton>Crear cuenta</Boton>
+                    </Link>   
+                </>
+            ) }
         </div>
       </ContenedorHeader>
     </header>
