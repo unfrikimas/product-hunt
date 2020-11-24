0b2ba0a0aad2d839f015a141157b7ef59f2601bf
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-24 18:35:30 +0100

    Creando el context de firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAjxjB8dp0hMI9mWiMlz9WH8xpHVZGUeAc",
+    authDomain: "product-hunt-7f0c7.firebaseapp.com",
+    databaseURL: "https://product-hunt-7f0c7.firebaseio.com",
+    projectId: "product-hunt-7f0c7",
+    storageBucket: "product-hunt-7f0c7.appspot.com",
+    messagingSenderId: "950249790462",
+    appId: "1:950249790462:web:34210feae14cd5524ffeaf"
+};
+
+export default firebaseConfig;
