4cdc32a539d933bc1184cb2ac3b577cb7cc7743f
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-27 22:40:51 +0100

    Mostrando el contenido y seccion de comentarios

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,10 +1,13 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '../../components/layouts/Layout';
+import { formatDistanceToNow, format } from 'date-fns'
+import { es } from 'date-fns/locale';
 import { FirebaseContext } from '../../firebase';
 import Error404 from '../../components/layouts/404';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { Campo, InputSubmit } from '../../components/ui/Formulario';
 
 const ContenedorProducto = styled.div`
     @media (min-width: 768px) {
@@ -12,6 +15,9 @@ const ContenedorProducto = styled.div`
         grid-template-columns: 2fr 1fr;
         column-gap: 2rem;
     }
+`;
+const TextoFecha = styled.p`
+    font-size: 1.4rem;
 `;
 
 const Producto = () => {
@@ -55,7 +61,29 @@ const Producto = () => {
                     `}>{nombre}</h1>
                     <ContenedorProducto>
                         <div>
-                            1
+                            {/* <TextoFecha>Publicado el {format(new Date(Date(creado)), 'MM/dd/yyyy')}</TextoFecha> */}
+                            <img src={urlimagen}  />
+                            <p>{descripcion}</p>
+                            <h2>Agrega tu comentario</h2>
+                            <form>
+                                <Campo>
+                                    <input 
+                                        type="text"
+                                        name="mensaje"
+                                    />
+                                </Campo>
+                                <InputSubmit 
+                                    type="submit"
+                                    value="Agregar comentario"
+                                />
+                            </form>
+                            <h2>Comentarios</h2>
+                            {comentarios.map(comentario => (
+                               <li>
+                                   <p>{comentario.nombbre}</p>
+                                   <p>{comentario.usuarioNombre}</p>
+                               </li> 
+                            ))}
                         </div>
                         <aside>
                             2
